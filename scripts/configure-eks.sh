51cd8f46e3127b023541ee97694710b04b083d46
Ubuntu <ubuntu@ip-172-31-30-37.ap-south-1.compute.internal>
2025-12-16 15:59:20 +0000

    my commit

--- scripts/configure-eks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Updating kubeconfig for EKS..."
-aws eks update-kubeconfig --region ap-south-1 --name brain-task-cluster
-
-kubectl get nodes
-
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,20 +6,39 @@ AWS_ACCOUNT_ID="213375652280"
 ECR_REPO="brain-task-app"
 IMAGE_TAG="latest"
 
+DEPLOYMENT_NAME="brain-task-deployment"
+CONTAINER_NAME="brain-task"
+SERVICE_NAME="brain-task-service"
+NAMESPACE="default"
+
 echo "Applying Kubernetes manifests..."
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 
 echo "Updating image..."
-kubectl set image deployment/brain-task-deployment \
-  brain-task=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO:$IMAGE_TAG \
+kubectl set image deployment/$DEPLOYMENT_NAME \
+  $CONTAINER_NAME=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO:$IMAGE_TAG \
+  -n $NAMESPACE \
   --record
 
 echo "Waiting for rollout..."
-kubectl rollout status deployment/brain-task-deployment
+kubectl rollout status deployment/$DEPLOYMENT_NAME -n $NAMESPACE
 
 echo "Current Pods:"
-kubectl get pods
+kubectl get pods -n $NAMESPACE
 
 echo "Service details:"
-kubectl get svc
+kubectl get svc $SERVICE_NAME -n $NAMESPACE
+
+echo "Waiting for LoadBalancer hostname..."
+LB_HOSTNAME=""
+
+# Wait until LoadBalancer hostname is assigned
+while [ -z "$LB_HOSTNAME" ]; do
+  LB_HOSTNAME=$(kubectl get svc $SERVICE_NAME \
+    -n $NAMESPACE \
+    -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+  sleep 5
+done
+
+echo "LoadBalancer Hostname: $LB_HOSTNAME"
